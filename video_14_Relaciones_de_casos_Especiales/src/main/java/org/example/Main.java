10db454f905418a1921308f800428910b45028ca
Cuervo <vidalcuervo@gmail.com>
2026-03-05 01:19:42 -0600

    video_14_Relaciones_de_casos_Especiales_Forma_1

---
+++ video_14_Relaciones_de_casos_Especiales/src/main/java/org/example/Main.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.example.entidades.Usuario;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        try(EntityManagerFactory emf =
+                    Persistence.createEntityManagerFactory(
+                "lab-persistence-unit"
+        )){
+            EntityManager em = emf.createEntityManager();
+
+            em.getTransaction().begin();
+
+            Usuario usuario = new Usuario(
+                    "Pedro Perez",
+                    "pedro@main.com",
+                    "1234",
+                    "Calle 123",
+                    "123456789",
+                    "amarillo",
+                    "abstracto");
+            em.persist(usuario);
+
+            em.getTransaction().commit();
+        }
+    }
+}
