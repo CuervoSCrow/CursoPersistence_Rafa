5219067e9fce57c82ca51cd8146acaf5757d8493
Cuervo <vidalcuervo@gmail.com>
2026-03-16 01:45:35 -0600

    video 18 JPQL Uniones Join en JPQL y HQL

---
+++ video_18_JPQL_Uniones_JOIN_en_JPQL_y_HQL/src/main/java/org/example/entidades/Alumno.java
@@ -0,0 +1,62 @@
+package org.example.entidades;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "alumnos")
+@NamedQuery(
+        name = "Alumno.findAll",
+        query="SELECT a FROM Alumno a ORDER BY a.nombre"
+)
+@Getter @Setter @NoArgsConstructor
+public class Alumno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+
+    @Column(name = "nombre",length = 50,nullable = false)
+    private String nombre;
+
+    @Column(name = "fecha_nacimiento",nullable = false)
+    private LocalDate fechaNac;
+
+    @Column(name = "email_alumno",unique = true)
+    private String email;
+
+    @Column(name = "nota_alumno",nullable = false)
+    private int nota;
+
+    @ManyToOne(optional = true,fetch = FetchType.LAZY)
+    @JoinColumn(name="id_curso")
+    private Curso curso;
+
+    public Alumno(String nombre,
+                  LocalDate fechaNac,
+                  String email,
+                  int nota,
+                  Curso curso) {
+        this.nombre = nombre;
+        this.fechaNac = fechaNac;
+        this.email = email;
+        this.nota = nota;
+        this.curso = curso;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", fechaNac=" + fechaNac +
+                ", email='" + email + '\'' +
+                ", nota=" + nota +
+                ", curso=" + curso +
+                '}';
+    }
+}
+
---
+++ video_18_JPQL_Uniones_JOIN_en_JPQL_y_HQL/src/main/java/org/example/entidades/Curso.java
@@ -0,0 +1,49 @@
+package org.example.entidades;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "cursos")
+@Getter @Setter @NoArgsConstructor
+public class Curso {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+
+    @Column(name = "nombre",nullable = false,length = 50)
+    private String nombre;
+
+    @Column(name = "nivel", nullable = false)
+    private int nivel;
+
+    @Column(name = "profesor", length = 50, nullable = false)
+    private String profesor;
+
+    @OneToMany(mappedBy = "curso",
+                fetch = FetchType.LAZY,
+                cascade = CascadeType.ALL//,
+//                orphanRemoval = true
+    )
+    private List<Alumno> alumnos;
+
+    public Curso(String nombre, int nivel, String profesor) {
+        this.nombre = nombre;
+        this.nivel = nivel;
+        this.profesor = profesor;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", nivel=" + nivel +
+                ", profesor='" + profesor + '\'' +
+                '}';
+    }
+}
